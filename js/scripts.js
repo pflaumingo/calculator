09111c541dd2292d8bea65caaac6d2a49582819f
Pflaumingo <pflauming@gmail.com>
2016-12-17 12:39:49 -0800

    Calculator with addition function

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(function() {
+  $("form#add").submit(function(event) {
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = (add(number1, number2));
+    $("#output").text(result);
+    event.preventDefault();
+  });
+});
